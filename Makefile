93f88ee98b86bd5ca9599871bea9d3ca051b087f
Woojeong Yang <wooyang@f4r11s7.clusters.42paris.fr>
2025-07-24 14:15:50 +0200

    garde tous les fichiers

--- Makefile
+++ Makefile
@@ -2,31 +2,31 @@ CC	= cc
 
 CFLAGS	= -Wall -Wextra -Werror
 
-SRC	=	main.c\
-		pipex_exec.c\
-		pipex_fork.c\
-		libft_petit.c\
+SRC	=	main.c \
+		pipex_exec.c \
+		pipex_fork.c \
+		libft_petit.c
 
 OBJ = ${SRC:.c=.o}
 
 %.o:%.c
-	${CC} ${CFLAGS} -c $^
+	${CC} ${CFLAGS} -c $< -o $@
 
 NAME	= pipex.a
 
 all : ${NAME}
 
 ${NAME} : ${OBJ}
-	ar -rcs -o $@ $^
+	ar -rcs $@ $^
 
 clean	:
-	rm -f	main.c\
-			pipex_exec.c\
-			pipex_fork.c\
-			libft_petit.c\
+	rm -f	main.o \
+			pipex_exec.o \
+			pipex_fork.o \
+			libft_petit.o
 
 fclean	:	clean
-	rm	-f	${NAME}
+	rm	-f	${OBJ}
 
 re	:	fclean
 	make
---
+++ pipex_fork.c
@@ -0,0 +1,43 @@
+#include "pipex.h"
+
+void    ft_fork(char ***argv, int nb, t_pipex *pipex)
+{
+    if (nb == 1)
+    {
+        pipex->inf = open((*argv)[nb], O_RDONLY);
+        if (pipex->inf == -1)
+        {
+            perror("infile");
+            exit (1);
+        }
+        if (dup2(pipex->inf, 0) == -1)
+        {
+            perror("dup2_inf");
+            exit (1);
+        }
+        if (dup2(pipex->pipefd[1], 1) == -1)
+        {
+            perror("dup2_pipefd[1]");
+            exit (1);
+        }
+        close(pipex->pipefd[0]);
+        close(pipex->pipefd[1]);
+        close(pipex->inf);
+    }
+    if (nb == 4)
+    {
+        pipex->outf = open((*argv)[nb], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        if (pipex->outf == -1)
+        {
+            perror("outfile");
+            exit (1);
+        }
+        if (dup2(pipex->outf, 1) == -1)
+        {
+            perror("outf");
+            exit (1);
+        }
+        close(pipex->pipefd[0]);
+        close(pipex->outf);
+    }
+}
